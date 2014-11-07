eeb587b0f0ba3a4a8b39c01366c1292d137091ee
Krzysztof Skarupa <krzysztof.skarupa@gmail.com>
2014-11-07 18:48:57 +0100

    Changed project name to somethig more generic

--- tmux_start.sh
+++ tmux_start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-tmux new-session -d -s hogarth
+tmux new-session -d -s project_name
 
-tmux new-window -t hogarth:1 -n 'Vim'
-tmux new-window -t hogarth:2 -n 'cli'
-tmux new-window -t hogarth:3 -n 'serv'
-tmux new-window -t hogarth:4 -n 'celery'
+tmux new-window -t project_name:1 -n 'Vim'
+tmux new-window -t project_name:2 -n 'cli'
+tmux new-window -t project_name:3 -n 'serv'
+tmux new-window -t project_name:4 -n 'celery'
 
-tmux select-window -t hogarth:1
-tmux attach -t hogarth
+tmux select-window -t project_name:1
+tmux attach -t project_name
